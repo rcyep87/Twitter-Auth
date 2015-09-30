e74df3962b7dff7ec63104176affbeb76f102dc1
Ryan Yep <ryan.yep@gmail.com>
2015-09-30 16:57:23 -0500

    db reset database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        forakermat.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
