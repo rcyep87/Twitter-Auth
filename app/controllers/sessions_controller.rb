0c081a6596c87f1cdd7e0800c9d431c63870b3d8
Ryan Yep <ryan.yep@gmail.com>
2015-10-02 15:54:14 -0500

    check in create action to see if user exists and authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController <  ApplicationController
   end
 
   def create #create the login session
-
+    user = User.find_by_username(params[:username])
+    if user.present? && user.authenticate(params[:password])
+      set_user_session(user)
+      redirect_to root_path, notice: "Successfully logged in."
+    else
+      flash[:alert] = "Username or Password did not match."
+      render :new
+    end
   end
 
   def destroy #delete the login session
