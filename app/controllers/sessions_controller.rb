6e7f074aa080ac7c6f4226d36132ed47c392eba6
Ryan Yep <ryan.yep@gmail.com>
2015-10-02 15:14:01 -0500

    added session actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,19 @@
 class SessionsController <  ApplicationController
+  skip_before_filter :authenticate_user
 
+  def new #login page
+  end
+
+  def create #create the login session
+
+  end
+
+  def destroy #delete the login session
+  end
+
+  private
+
+  def set_user_session(user) #places temporary cookie on user's browser
+    session[:logged_in_user] = user.id
+  end
 end
