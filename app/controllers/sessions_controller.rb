a715a675dd739a9c67bed0cd96d9653221601333
Ryan Yep <ryan.yep@gmail.com>
2015-10-03 10:25:38 -0500

    Successful re_direct to user_path after successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController <  ApplicationController
     user = User.find_by_username(params[:username])
     if user.present? && user.authenticate(params[:password])
       set_user_session(user)
-      redirect_to root_path, notice: "Successfully logged in."
+      redirect_to user_path(user), notice: "Successfully logged in."
     else
       flash[:alert] = "Username or Password did not match."
       render :new
